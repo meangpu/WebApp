b29a067f9e9a4bd78c59bf5c6e534de631cebd69
meangpu <51825872+meangpu@users.noreply.github.com>
2020-01-22 17:06:02 +0700

    yeah

---
+++ lab6/exercise1/scripts.js
@@ -0,0 +1,7 @@
+var person = { firstname: "John", lastname: "Doe" };
+var word = document.getElementById("answer");
+
+word.textContent = "Firstname is " + person.firstname;
+
+// document.write("Hello World!");
+// cannot be use because of async
---
+++ lab6/exercise2/scripts.js
@@ -0,0 +1,4 @@
+var person = { firstname: "Manee", lastname: "Doe", country: "Thailand" };
+var word = document.getElementById("answer");
+
+word.textContent += "Firstname is " + person.firstname + " is in " + person.country;
