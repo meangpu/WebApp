ea8ef8fb0195c9bb2b8e6bace73b0283639aa039
meangpu <51825872+meangpu@users.noreply.github.com>
2020-01-22 17:16:18 +0700

    as

--- lab6/exercise1/scripts.js
+++
@@ -1,7 +0,0 @@
-var person = { firstname: "John", lastname: "Doe" };
-var word = document.getElementById("answer");
-
-word.textContent = "Firstname is " + person.firstname;
-
-// document.write("Hello World!");
-// cannot be use because of async
