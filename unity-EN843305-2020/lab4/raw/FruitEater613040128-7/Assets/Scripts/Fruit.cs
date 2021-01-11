5abf61bc57d3718b8c82c8f94d07b1b955f2cad2
meangpu <meangpu@gmail.com>
2021-01-11 15:56:05 +0700

    lab4 part 1

--- unity-EN843305-2020/lab4/raw/FruitEater613040128-7/Assets/Scripts/Fruit.cs
+++ unity-EN843305-2020/lab4/raw/FruitEater613040128-7/Assets/Scripts/Fruit.cs
@@ -1,24 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Fruit : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void OnCollisionEnter2D(Collision2D other) {
+        
         if(other.gameObject.name == "player"){
+            GameObject.Find("EventSystem").GetComponent<scoreManager>().score += 1;
             Destroy(gameObject);
+            GameObject.Find("EventSystem").GetComponent<scoreManager>().updateScore();
         }
         
     }
---
+++ unity-EN843305-2020/lab4/raw/FruitEater613040128-7/Assets/Scripts/scoreManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class scoreManager : MonoBehaviour
+{
+    public int score;
+    public TextMeshProUGUI text_score;
+
+    public void updateScore(){
+        text_score.text = "score: " + score.ToString();
+    }
+
+
+}
