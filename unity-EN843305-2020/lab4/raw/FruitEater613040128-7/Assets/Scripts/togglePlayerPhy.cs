13b6831905d7379c7383eb71e2006307e3803dff
meangpu <meangpu@gmail.com>
2021-01-11 14:47:16 +0700

    lab4 add fruit

---
+++ unity-EN843305-2020/lab4/raw/FruitEater613040128-7/Assets/Scripts/togglePlayerPhy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class togglePlayerPhy : MonoBehaviour
+{
+     public PhysicsMaterial2D Material1;
+     //in the editor this is what you would set as the object you wan't to change
+
+     public void changeMat()
+     {
+        Material1.bounciness = 0;
+     }
+ 
+}
