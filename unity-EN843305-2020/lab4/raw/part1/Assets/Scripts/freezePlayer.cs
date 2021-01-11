9290666b3f2010788148a884cb42f6d292a1afa7
meangpu <meangpu@gmail.com>
2021-01-11 16:06:08 +0700

    upload game

---
+++ unity-EN843305-2020/lab4/raw/part1/Assets/Scripts/freezePlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class freezePlayer : MonoBehaviour
+{
+    public GameObject player;
+    public TextMeshProUGUI textButtonFreeze;
+    private bool freeze = false;
+
+    public void changePlayerState(){
+        if (!freeze) {
+            player.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+            textButtonFreeze.text = "Unfreeze player";
+            freeze = true;
+        }
+        else{
+            player.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+            textButtonFreeze.text = "Freeze player";
+            freeze = false;
+        }
+    }
+
+}
