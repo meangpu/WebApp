30c312115db4fec556c489a01853b680e40d4bd7
meangpu <meangpu@gmail.com>
2021-01-14 09:27:34 +0700

    hello world print

---
+++ unity-EN843305-2020/pGap/BasicCoding/Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    void Start()
+    {
+        print("Hellow World");
+        // Debug.Log("Hello World");
+    }
+
+    void Update()
+    {
+
+    }
+}
