cb23083e8b859f56f6e5c0510512d021256f6cda
meangpu <meangpu@gmail.com>
2021-01-14 10:07:29 +0700

    shift a

--- unity-EN843305-2020/pGap/BasicCoding/Assets/Scripts/position.cs
+++ unity-EN843305-2020/pGap/BasicCoding/Assets/Scripts/position.cs
@@ -4,24 +4,39 @@ using UnityEngine;
 
 public class position : MonoBehaviour
 {
-    public int movementSpeed;
+    // public int movementSpeed;
+    public Vector3 speedUpdate;
     
     void Start()
     {
-
+        // transform.position = new Vector3(5, 2, 0);
     }
 
     void Update()
     {
-        //get the Input from Horizontal axis
-        float horizontalInput = Input.GetAxis("Horizontal");
-        //get the Input from Vertical axis
-        float verticalInput = Input.GetAxis("Vertical");
 
-        //update the position
-        transform.position = transform.position + new Vector3(horizontalInput * movementSpeed * Time.deltaTime, verticalInput * movementSpeed * Time.deltaTime, 0);
+        // //get the Input from Horizontal axis
+        // float horizontalInput = Input.GetAxis("Horizontal");
+        // //get the Input from Vertical axis
+        // float verticalInput = Input.GetAxis("Vertical");
 
-        //output to log the position change
-        Debug.Log(transform.position);
+        // //update the position
+        // transform.position = transform.position + new Vector3(horizontalInput * movementSpeed * Time.deltaTime, verticalInput * movementSpeed * Time.deltaTime, 0);
+
+        // //output to log the position change
+        // Debug.Log(transform.position);
+
+        // Input.GetKeyDown();
+
+        if (Input.GetKeyDown(KeyCode.A) && (Input.GetKey(KeyCode.LeftShift))){
+            print("big A key was pressed!!");
+        }
+ 
+
+
+        transform.position += speedUpdate;
+
+
+
     }
 }
