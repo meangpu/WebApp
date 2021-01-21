44cb771762d997532ec43226aa4ed2c85c5af2ec
meangpu <meangpu@gmail.com>
2021-01-21 11:01:08 +0700

    update player control script

--- unity-EN843305-2020/lab5/raw/Assets/Scripts/PlayerControl.cs
+++ unity-EN843305-2020/lab5/raw/Assets/Scripts/PlayerControl.cs
@@ -5,18 +5,78 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour
 {
 
+
     public bool isLeft;
     public bool isRight;
     public bool isJump;
     Rigidbody2D rb;
 
-    void Start()
-    {
-        
+
+    
+
+    public void SetLeft(bool b){
+        isLeft = b;
+    }
+    public void SetRight(bool b){
+        isRight = b;
+    }
+    public void SetJump(bool b){
+        isJump = b;
     }
 
-    void Update()
+
+    void KeyboardControl()
     {
-        
+        if (Input.GetKeyDown("a")){
+            SetLeft(true);
+        }
+        if (Input.GetKeyUp("a")){
+            SetLeft(false);
+        }
+        if (Input.GetKeyDown("d")){
+            SetRight(true);
+        }
+        if (Input.GetKeyUp("d")){
+            SetRight(false);
+        }
+        if (Input.GetKeyDown("space")){
+            SetJump(true);
+        }
     }
+
+
+
+    void Start() 
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        KeyboardControl();
+
+        if (isLeft)
+        {
+            rb.AddForce(Vector2.left *10);
+        }
+        else if (isRight)
+        {
+            rb.AddForce(Vector2.right *10);
+        }
+
+
+        if (isJump)
+        {
+            rb.AddForce(Vector2.up * 200);
+            isJump = false;
+        }
+
+
+    }
+
+    
+
+
+
+
+
 }
