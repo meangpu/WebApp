cb38f863e901f433569a78a5dd5db7b11f5a9cb6
meangpu <meangpu@gmail.com>
2021-01-21 10:25:27 +0700

    #1 add Script name “PlayerControl”

---
+++ unity-EN843305-2020/lab5/raw/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+
+    public bool isLeft;
+    public bool isRight;
+    public bool isJump;
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
