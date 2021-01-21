bfd4b282b5a38ff358f830170513ddd5212fff92
meangpu <meangpu@gmail.com>
2021-01-22 00:33:45 +0700

    update lab1 lab 2

---
+++ unity-EN843305-2020/lab2/raw/Assets/Creator Kit - Beginner Code/Scripts/Tutorial/SpawnerSample.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using CreatorKitCode;
+
+
+public class SpawnerSample : MonoBehaviour
+{
+    public GameObject ObjectToSpawn;
+    public int radi = 3;
+
+    public class LootAngle
+    {
+        int angle;
+        int step;
+
+        public LootAngle(int increment)
+        {
+            step = increment;
+            angle = 0;
+        }
+
+        public int NextAngle()
+        {
+            int currentAngle = angle;
+            angle = Helpers.WrapAngle(angle + step);
+
+            return currentAngle;
+        }
+    }
+
+
+   void SpawnPotion(int angle)
+    {
+        Vector3 direction = Quaternion.Euler(0, angle, 0) * Vector3.right;
+        Vector3 spawnPosition = transform.position + direction * radi;
+        Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
+    }
+
+    void Start()
+    {
+        LootAngle myLootAngle = new LootAngle(45);
+
+        //every call will advance the angle!
+        SpawnPotion(myLootAngle.NextAngle());
+        SpawnPotion(myLootAngle.NextAngle());
+        SpawnPotion(myLootAngle.NextAngle());
+        SpawnPotion(myLootAngle.NextAngle());
+    }
+}
+
