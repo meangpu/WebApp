11761cf48f78c933fb1069832309c6f2a9275e0c
meangpu <51825872+meangpu@users.noreply.github.com>
2021-02-25 11:11:25 +0700

    midterm

---
+++ unity-EN843305-2020/Project/midtermExam/nattapong-midterm-exam-2021/Assets/Script/3d/Fruit.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fruit : MonoBehaviour
+{
+    public GameManager gameManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision other) {
+        if(other.gameObject.tag == "Player"){
+         
+        }
+    }
+}
---
+++ unity-EN843305-2020/Project/midtermExam/nattapong-midterm-exam-2021/Assets/Script/3d/GameManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public int gameTime = 99;
+    public int score = 0;
+    public GameObject EndScreen;
+    
+
+    public TextMeshProUGUI gameTimeText;
+    public TextMeshProUGUI endGameScore;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI endGameComment;
+    
+    void Start()
+    {
+        Time.timeScale = 1.0f;
+        scoreText.text = score.ToString();
+        gameTimeText.text = gameTime.ToString();
+        InvokeRepeating("CountDown", 1.0f, 1.0f);
+    }
+
+    public void gameover(string death_message){
+        endGameComment.text = death_message;
+        Pause();
+        EndScreen.gameObject.SetActive(true);
+    }
+
+
+    public void AddScore(int s)
+    {
+        score += s;
+        scoreText.text = score.ToString();
+        endGameScore.text = score.ToString();
+
+    }
+
+    public void restartGame()
+    {
+        EndScreen.gameObject.SetActive(false);
+        SceneManager.LoadScene("Game");
+    }
+
+    public void CountDown(){
+        if(gameTime > 0){
+            gameTime --;
+        }
+        else{
+            gameover("Time is up!");
+        }
+        gameTimeText.text = gameTime.ToString();
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+    }
+}
